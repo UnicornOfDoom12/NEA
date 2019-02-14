8d72773892d7e71361d972f58c7d119f49695a08
JoeM <joe.george.malley@gmail.com>
2019-02-14 16:09:42 +0000

    Health Counter

--- Assets/Scripts/PlayerDeathHandler.cs
+++ Assets/Scripts/PlayerDeathHandler.cs
@@ -1,21 +1,31 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Mono.Data;
+using Mono.Data.Sqlite;
+using System.Text;
+using UnityEngine.UI;
 using UnityEngine;
+using System.Threading; // imports including sqlite
 
 public class PlayerDeathHandler : MonoBehaviour {
 	public int Health;
 	public AudioSource SoundSource;
 	public AudioClip DeathClip;
 	public LoadNewScene LoadNewScene;
+	public Text HealthCounter;
 	// Use this for initialization
 	void Start () {
 		Health = 100;
+		HealthCounter.text = "Health = "+ Health.ToString() + "/100";
 	}
 	public void TakeDamage(int Damage){
 		Health = Health - Damage;
 		if (Health <= 0){
 			KillPlayer();
 		}
+		HealthCounter.text = "Health = "+ Health.ToString() + "/100";
 	}
 	void KillPlayer(){
 		SoundSource.clip = DeathClip;
@@ -27,6 +37,7 @@ public class PlayerDeathHandler : MonoBehaviour {
 		if (Health > 100){
 			Health = 100;
 		}
+		HealthCounter.text = "Health = "+ Health.ToString() + "/100";
 
 	}
 	

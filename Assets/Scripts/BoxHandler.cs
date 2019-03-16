630ce886c55bc506012e033325e5b928e1f4d07e
JoeM <joe.george.malley@gmail.com>
2019-03-16 11:44:08 +0000

    coolio

--- Assets/Scripts/BoxHandler.cs
+++ Assets/Scripts/BoxHandler.cs
@@ -11,8 +11,7 @@ public class BoxHandler : MonoBehaviour {
 
 
 	public GameObject Chest;
-	public SpriteRenderer SpriteRender; // declare sprite render component
-	public bool BoxColourDecided = false;
+	//public SpriteRenderer SpriteRender; // declare sprite render component
 	public Sprite sprite0; // the following variables are all different version of the chest sprite
 	public Sprite sprite1;
 	public Sprite sprite2;
@@ -35,7 +34,7 @@ public class BoxHandler : MonoBehaviour {
 		print("Determine Presence");
 		int Cordx = CordinateHandler.Cordx;
 		int Cordy = CordinateHandler.Cordy;
-		SpriteRender = this.gameObject.GetComponent<SpriteRenderer>(); // imports the game object used for the sprite render variable
+		//SpriteRender = this.gameObject.GetComponent<SpriteRenderer>(); // imports the game object used for the sprite render variable
 		SpriteArray = new List<Sprite>{sprite0,sprite1,sprite2,sprite3,sprite4,sprite5,sprite6,sprite7,sprite8,sprite9,sprite10,sprite11}; // Creates an array of the different sprites to be used
 		var RoomDB = new SqliteConnection("Data Source=Assets\\Plugins\\Rooms Table.db;Version=3;"); // defines the connection to database
 		RoomDB.Open(); // opens the connection
@@ -46,10 +45,11 @@ public class BoxHandler : MonoBehaviour {
 		using (var reader = CMD.ExecuteReader()){ // executes the command
 			bool Present = Convert.ToBoolean(reader["Box"]); // assigns the value in the database to a local variable
 			// TODO change this to be based on difficulty
-			 // changes the sprite version to the one defined by the random number
+			// changes the sprite version to the one defined by the random number
 			Vector2Int TempBoxVector = new Vector2Int(Cordx,Cordy);
 			if (Present){ // if there is a box in the room then move the box into view
 				GameObject TempChest = Instantiate(Chest,transform.position,transform.rotation);
+				SpriteIndex = UnityEngine.Random.Range(0,5);
 				WeaponGenerate SpriteImage = TempChest.GetComponent<WeaponGenerate>();
 				SpriteImage.Closed = SpriteArray[SpriteIndex];
 				SpriteImage.Open = SpriteArray[SpriteIndex + 6];
@@ -67,7 +67,4 @@ public class BoxHandler : MonoBehaviour {
 			Destroy(i);
 		}
 	}
-	
-
-
 }

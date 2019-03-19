24fc895be6e7273d4aa8d09321e4a5cdc95e62c6
Joe Malley <bubb.bubbles.backup@googlemail.com>
2019-03-19 09:10:03 +0000

    Bigg offf

--- Assets/Scripts/ReorderIDs.cs
+++ Assets/Scripts/ReorderIDs.cs
@@ -10,34 +10,32 @@ using UnityEngine;
 using System.Threading; // imports including sqlite
 
 public class ReorderIDs : MonoBehaviour {
-
-	// Use this for initialization
 	void Awake () {
 		SqliteConnection WeaponDB = new SqliteConnection("Data Source=Assets\\Plugins\\WeaponsTable.db;Version=3;");
-		WeaponDB.Open();
-		List<int> NewIds = new List<int>();
+		WeaponDB.Open(); // Opens connection to the DB
+		List<int> NewIds = new List<int>(); // Creates the NewIds list
 		string CMDString1 = "SELECT COUNT(*) FROM tblWeapon";
 		SqliteCommand CMD1 = new SqliteCommand(CMDString1,WeaponDB);
 		int Data = int.Parse(CMD1.ExecuteScalar().ToString());
 		for (int i = 1; i <= Data; i++){
-			NewIds.Add(i);
+			NewIds.Add(i); // Creates a list of increasing integers up to the amount of records
 		}
-		List<int> OldIds = new List<int>();
+		List<int> OldIds = new List<int>(); // creates a list of all the current Ids in ascending order
 		string CMDString2 = "SELECT id FROM tblWeapon";
 		SqliteCommand CMD2 = new SqliteCommand(CMDString2, WeaponDB);
 		SqliteDataReader read = CMD2.ExecuteReader();
 		while (read.Read()){
-			OldIds.Add(Convert.ToInt32(read["id"]));
+			OldIds.Add(Convert.ToInt32(read["id"])); // Populates the list with the Ids
 		}		
 		int x = 1;
 		for(int i = 1; i <= Data; i++){
 			string CMDString3 = "UPDATE tblWeapon SET id=@newid WHERE id=@i";
 			SqliteCommand CMD3 = new SqliteCommand(CMDString3,WeaponDB);
 			CMD3.Parameters.AddWithValue("@newid", NewIds[x - 1]);
-			CMD3.Parameters.AddWithValue("@i",OldIds[i - 1]);
+			CMD3.Parameters.AddWithValue("@i",OldIds[i - 1]); // This loop replaces OldIDs with new Ids
 			CMD3.ExecuteNonQuery();
 			x += 1;
 		}	
-		WeaponDB.Close();
+		WeaponDB.Close(); // Closes connection
 	}
 }

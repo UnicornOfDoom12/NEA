8e99b125643964a88acd6a1538fbcb869aa1163f
Joe Malley <bubb.bubbles.backup@googlemail.com>
2019-03-27 10:01:05 +0000

    comments, probably

--- Assets/Scripts/DifficultyScoreTracker.cs
+++ Assets/Scripts/DifficultyScoreTracker.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DifficultyScoreTracker : MonoBehaviour {
-
-	// Use this for initialization
 	public float DifficultyScore;
 	public float FinalScore;
 	public Weapon Weapon;
 	public CordinateHandler CordinateHandler;
-
-	void Start () {
-		DifficultyScore = 1;
-		if (Weapon.Category == "SMG"){
+	void Start () { // run at the start of the program
+		DifficultyScore = 1; // initial value
+		if (Weapon.Category == "SMG"){ // determines the category for each weapon then divides by the base value to get how much it is over
 			DifficultyScore += Weapon.Damage / 25;
 		}
 		else if (Weapon.Category == "Assault Rifle"){
@@ -23,10 +20,9 @@ public class DifficultyScoreTracker : MonoBehaviour {
 		}
 		else if (Weapon.Category == "Marksman Rifle"){
 			DifficultyScore += Weapon.Damage / 70;
-		}
+		} // this function will set the DifficultyScore variable to a base value
 	}
-	
 	public void ChangeScore(){
-		FinalScore = DifficultyScore + (CordinateHandler.Cordx + CordinateHandler.Cordy)/15;
+		FinalScore = DifficultyScore + (CordinateHandler.Cordx + CordinateHandler.Cordy)/15; // Every time the player changes room the final score gets calculated
 	}
 }

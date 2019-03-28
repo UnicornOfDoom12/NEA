d32e26972ac757d67b213d300183d2f2090a5090
Joe Malley <bubb.bubbles.backup@googlemail.com>
2019-03-28 10:08:14 +0000

    So riggidy diggidy changes, and commnets

--- Assets/Scripts/MapMarkerHandler.cs
+++ Assets/Scripts/MapMarkerHandler.cs
@@ -8,9 +8,9 @@ public class MapMarkerHandler : MonoBehaviour {
 		CordinateHandler CordinateHandler = Tracker.GetComponent<CordinateHandler>(); // adds the script with the values of cordx inside
 		int Cordx = CordinateHandler.Cordx; // assigns the values of cordx to a local variable
 		int Cordy = CordinateHandler.Cordy; // assigns the values of cordx to a local variable
-		float posx = -50 + -6.5f + (4 * Cordx);
-		float posy = 3.0f - (2 * Cordy);
-		Vector3 NewPosition = new Vector3(posx,posy,0);
-		transform.position = NewPosition;
+		float posx = -50 + -6.5f + (4 * Cordx); // adjust the position of the marker in the x direction
+		float posy = 3.0f - (2 * Cordy); // adjust the position of the marker in the y direction
+		Vector3 NewPosition = new Vector3(posx,posy,0); // Converts the position into a vector
+		transform.position = NewPosition; // assigns the vector
 	}
 }

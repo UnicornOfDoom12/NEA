26b14a381b8c7aba55ac2f8bd142899eebebfdcd
JoeM <joe.george.malley@gmail.com>
2019-04-01 14:15:13 +0100

    some riggidy diggidy

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -14,18 +14,18 @@ public class PlayerShoot : MonoBehaviour {
 	public Rigidbody2D Projectile;
 	public Animator Animator;
 	public float ProjectileSpeed;
-	private float FireRate;
-	private float TimeSinceFire;
-	private int CurrentAmmo;
+	public float FireRate;
+	public float TimeSinceFire;
+	public int CurrentAmmo;
 	public AudioSource SoundSource;
 	public AudioClip ReloadClip;
 	public AudioClip ShootClip;
 	public GameObject Barrel;
 	public GameObject MuzzleAnimation;
-	private bool Reloading;
-	private float ReloadTime = 2;
-	private float Timer = 2.1f;
-	private float Inaccuracy;
+	public bool Reloading;
+	public float ReloadTime = 2;
+	public float Timer = 2.1f;
+	public float Inaccuracy;
 	public float MeleeTimer = 0.7f;
 	public float MeleeCooldown = 0.6f;
 	private bool MeleeAttacking = false;
@@ -36,171 +36,143 @@ public class PlayerShoot : MonoBehaviour {
 	public ExitHandler ExitHandler;
 	public CordinateHandler CordinateHandler;
 	public AudioClip OpenDoor;
-	void Start(){
+	void Start(){ // Run at the start
 		FireRate = Weapon.FireRate;
 		FireRate = FireRate / 60;
-		FireRate = 1/FireRate;
-		CurrentAmmo = Weapon.Magazine;
-		Reloading = false;
-		Inaccuracy = Weapon.Inaccuracy;
-		MeleeAttacking = false;
-		AmmoCounter.text = "Ammo = " + CurrentAmmo.ToString() + " / " + Weapon.Magazine.ToString();
-		Timer = 3;
-		MeleeTimer = 0.7f;
+		FireRate = 1/FireRate; // Changes the firerate from shots per minute into time between shots
+		CurrentAmmo = Weapon.Magazine; // Set to full at the start of the scene
+		Reloading = false; // set to false at the start
+		Inaccuracy = Weapon.Inaccuracy; // Determines the inaccuracy
+		MeleeAttacking = false; // Set to false at the start
+		AmmoCounter.text = "Ammo = " + CurrentAmmo.ToString() + " / " + Weapon.Magazine.ToString(); // Sets the ammo counter text
+		Timer = 3; // reload timer, set to aribitary value above 2.1
+		MeleeTimer = 0.7f; // Set to a value above 0.6
 	}
-	// Update is called once per frame
-	void FixedUpdate () {
-		float Passed = Time.deltaTime;
+	void FixedUpdate () { // Run at the same time every frame
+		float Passed = Time.deltaTime; // Set to the time that has been passes since last call
 		if (Reloading){
-			Timer += Passed;
+			Timer += Passed; // adds the time past to the timer
 		}
 		if(MeleeAttacking){
-			MeleeTimer += Passed;
+			MeleeTimer += Passed; // adds the time past to the timer
 		}
-		if (MeleeTimer >= MeleeCooldown){
+		if (MeleeTimer >= MeleeCooldown){ // if they have meleed for long enough then they are not attacking anymore
 			Animator.SetBool("IsMelee",false);
 			MeleeAttacking = false;
 		}
-		if(Timer >= ReloadTime){
+		if(Timer >= ReloadTime){//if they have reloaded for long enough then they are not reloading anymore
 			Animator.SetBool("IsReloading",false);
 		}
-
-		if (Input.GetMouseButton(0)){
-			Animator.SetBool("IsShooting",false);
-			if (Time.time >= TimeSinceFire){
-				TimeSinceFire = Time.time + FireRate;
-				Fire();
-			}
-			else{
-				//print("Attempted to fire " + Weapon.FireRate.ToString());
+		if (Input.GetMouseButton(0)){ // If the user presses left mouse button
+			Animator.SetBool("IsShooting",false); // set to false as a base
+			if (Time.time >= TimeSinceFire){ // If it has been long enough since the last shot then fire again
+				TimeSinceFire = Time.time + FireRate; // add the fire rate to the timer
+				Fire(); // run the fire function
 			}
 		}
-		if (Input.GetKeyDown(KeyCode.V)){
-			MeleeAttack();
+		if (Input.GetKeyDown(KeyCode.V)){ // if user presses V
+			MeleeAttack(); // melee attack
 		}
-		if (Input.GetKeyDown(KeyCode.R)){
-			Reload();
+		if (Input.GetKeyDown(KeyCode.R)){ // if user presses R
+			Reload(); // reload
 		}
 	}
-	void Fire(){
-		
-		bool CanShoot = true;
-		if (Timer < ReloadTime){
-			print(Timer);
-			print("Still reloading");
+	void Fire(){ // Ran when user presses left click
+		bool CanShoot = true; // Is set to true until proven false
+		if (Timer < ReloadTime){ // If they are still reloading, set it to false
 			CanShoot = false;
 		}
 		else{
-			Reloading = false;
+			Reloading = false; // if not reloading, reloading = false
 		}
-		if (MeleeTimer < MeleeCooldown){
-			print(MeleeTimer);
-			print("Still meleeing");
+		if (MeleeTimer < MeleeCooldown){ // if they are meleeing then we cant shoot
 			CanShoot = false;
 		}
-		if (Weapon.Category == ""){
-			print("You got a knife so you melee attack");
+		if (Weapon.Category == ""){ // if they have a knife, melee attack instead
 			CanShoot = false;
 			MeleeAttack();
 		}
-		else if (CurrentAmmo <= 0){
-			
+		else if (CurrentAmmo <= 0){ // if they have no ammo, reload
 			CanShoot = false;
 			Reload();
 		}
-		if (CanShoot){
-
-			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			Rigidbody2D FiredBullet = Instantiate(Projectile, Barrel.transform.position,Quaternion.LookRotation(transform.position - mousePos, Vector3.forward)) as Rigidbody2D;
-			Vector3 BulletDirection = transform.right;
-			BulletDirection.x = BulletDirection.x + UnityEngine.Random.Range(-1 * (Inaccuracy/75),(Inaccuracy/75));
-			BulletDirection.y = BulletDirection.y + UnityEngine.Random.Range(-1 * (Inaccuracy/75),(Inaccuracy/75));
-			FiredBullet.AddForce(BulletDirection * ProjectileSpeed);
-			FiredBullet.name = "PlayerProjectile";
-			CurrentAmmo -= 1;
-			SoundSource.clip = ShootClip;
+		if (CanShoot){ // if they can shoot
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Get the mouse position
+			Rigidbody2D FiredBullet = Instantiate(Projectile, Barrel.transform.position,Quaternion.LookRotation(transform.position - mousePos, Vector3.forward)) as Rigidbody2D;// instantiate a projectile prefab
+			Vector3 BulletDirection = transform.right; // Direction = (1,0,0)
+			BulletDirection.x = BulletDirection.x + UnityEngine.Random.Range(-1 * (Inaccuracy/75),(Inaccuracy/75)); // Changes the direction based on inaccuracy values
+			BulletDirection.y = BulletDirection.y + UnityEngine.Random.Range(-1 * (Inaccuracy/75),(Inaccuracy/75)); 
+			FiredBullet.AddForce(BulletDirection * ProjectileSpeed);// add a force to the projectile in the new direction based on the inaccuracy
+			FiredBullet.name = "PlayerProjectile"; // Set the name to PlayerPorjectile for later identification
+			CurrentAmmo -= 1; // reduce the ammo by 1
+			SoundSource.clip = ShootClip; // play the sound file of the shooting
 			SoundSource.Play();
-			Animator.SetBool("IsShooting",true);
-			print("BANG you have " + CurrentAmmo.ToString());
-			var NewFlash = Instantiate(MuzzleAnimation, Barrel.transform.position,Barrel.transform.rotation);
-			Destroy(NewFlash,0.183f);
-			AmmoCounter.text = "Ammo = " + CurrentAmmo.ToString() + " / " + Weapon.Magazine.ToString();
-			
-
+			Animator.SetBool("IsShooting",true); // set animation of shooting to be true
+			var NewFlash = Instantiate(MuzzleAnimation, Barrel.transform.position,Barrel.transform.rotation); // Instantiate a muzzle flash object
+			Destroy(NewFlash,0.183f); // destroy it in 0.183 seconds (the amount of time for it to complete an animation)
+			AmmoCounter.text = "Ammo = " + CurrentAmmo.ToString() + " / " + Weapon.Magazine.ToString(); // Change the text
 		}
-
 	}
-	void MeleeAttack(){
-		// Play sound
-		print("doing melee");
-		Animator.SetBool("IsMelee", true);
-		MeleeAttacking = true;
-		MeleeTimer = 0;
+	void MeleeAttack(){ // Melee attack script
+		Animator.SetBool("IsMelee", true); // set the animation boolean to true
+		MeleeAttacking = true; // set melee attacking to true
+		MeleeTimer = 0; // the timer now = 0
 		Vector2 RayDir = new Vector2(0,transform.rotation.z);
-		RaycastHit2D Attack = Physics2D.Raycast(Barrel.transform.position, RayDir);
-		if (Attack.collider != null && Attack.distance <= 0.5f && Attack.collider != PlayerCollider){
-			
-			SoundSource.clip = MeleeClipHit;
-			print("Hit a " + Attack.collider.ToString());
-			if (Attack.collider.tag == "Finish"){
-				ExitHandler.Win();
+		RaycastHit2D Attack = Physics2D.Raycast(Barrel.transform.position, RayDir);// cast a ray forward from the player
+		if (Attack.collider != null && Attack.distance <= 0.5f && Attack.collider != PlayerCollider){ // if the ray hits something closer than 0.5 from it, and it isnt hitting itself then:
+			SoundSource.clip = MeleeClipHit; // Sets the sound clip to the melee clip
+			if (Attack.collider.tag == "Finish"){ // if enemy hits the end object, finish the game
+				ExitHandler.Win(); // executes the win function in win
 			}
-			if (Attack.collider.name == "North"){
-				print("Move north");
-				CordinateHandler.MoveNorth();
-				transform.position = new Vector3(0.0f, -4.35f, 0.0f);
-				SoundSource.clip = OpenDoor;
+			if (Attack.collider.name == "North"){ // If the user hits the north door
+				CordinateHandler.MoveNorth(); // run move north
+				transform.position = new Vector3(0.0f, -4.35f, 0.0f); // change player position to appear at the bottom of the screen
+				SoundSource.clip = OpenDoor; // play the door opening sound
 				SoundSource.Play();
 			}
-			if (Attack.collider.tag == "Box"){
+			if (Attack.collider.tag == "Box"){ // if the player hits a box, run the open box functions
 				WeaponGenerate Box = Attack.collider.gameObject.GetComponent<WeaponGenerate>();
 				Box.OpenBox();
 			}
-			if (Attack.collider.name == "South"){
-				print("Move South");
+			if (Attack.collider.name == "South"){ // if the player hits the south object move them south
 				CordinateHandler.MoveSouth();
 				transform.position = new Vector3(0.0f, 4.35f, 0.0f);
 				SoundSource.clip = OpenDoor;
 				SoundSource.Play();
 			}
-			if (Attack.collider.name == "East"){
-				print("Move East");
+			if (Attack.collider.name == "East"){ // if the player hits east move the player east
 				CordinateHandler.MoveEast();
 				transform.position = new Vector3(-10.0f, 0.0f, 0.0f);
 				SoundSource.clip = OpenDoor;
 				SoundSource.Play();
 			}
-			if (Attack.collider.name == "West"){
-				print("Move West");
+			if (Attack.collider.name == "West"){ // if the player hits west, move the player west
 				CordinateHandler.MoveWest();
 				transform.position = new Vector3(10.0f, 0.0f, 0.0f);
 				SoundSource.clip = OpenDoor;
 				SoundSource.Play();
 			}
-			if (Attack.collider.tag == "Enemy"){
+			if (Attack.collider.tag == "Enemy"){ // if the player hits an enemy make them take damage
 				var Turret = Attack.collider.gameObject.GetComponent<TurretHandler>();
 				Turret.TakeDamage(25);
 			}
-			if (Attack.collider.tag == "Monster"){
+			if (Attack.collider.tag == "Monster"){ // if the player hits a monster make them take damage
 				var Monster = Attack.collider.gameObject.GetComponent<MonsterHandler>();
 				Monster.TakeDamage(25);
 			}
 		}
-
 		else{
-			SoundSource.clip = MeleeClipMiss;
+			SoundSource.clip = MeleeClipMiss; // if they dont hit anything play the sound of them missing
 		}
-		SoundSource.Play();
+		SoundSource.Play(); // play the sound
 	}
-	void Reload(){
-		Reloading = true;
-		Timer = 0;
-		print("reload old ammo = " + CurrentAmmo.ToString());
-		SoundSource.clip = ReloadClip;
+	void Reload(){ // reload function
+		Reloading = true; // Realoading = true
+		Timer = 0; // reset the timer
+		SoundSource.clip = ReloadClip; // play the sound
 		SoundSource.Play();
-		Animator.SetBool("IsReloading",true);
-		CurrentAmmo = Weapon.Magazine;
-		print("New ammo = " + CurrentAmmo.ToString());
-		AmmoCounter.text = "Ammo = " + CurrentAmmo.ToString() + " / " + Weapon.Magazine.ToString();
+		Animator.SetBool("IsReloading",true); // set the animation
+		CurrentAmmo = Weapon.Magazine; // reset the ammo
+		AmmoCounter.text = "Ammo = " + CurrentAmmo.ToString() + " / " + Weapon.Magazine.ToString(); // update the counter
 	}
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,8 +10,7 @@ using UnityEngine;
 using System.Threading; // imports including sqlite
 
 public class Weapon : MonoBehaviour {
-	Animator animator;
-	public int WeaponId;
+	public int WeaponId; 
 	public string Category;
 	public int Damage;
 	public int Magazine;
@@ -19,25 +18,24 @@ public class Weapon : MonoBehaviour {
     public float Inaccuracy;
 	public PlayerMovement PlayerMovement;
 	public SelectedEquip SelectedEquip;
-	void Start(){
-		animator = GetComponent<Animator>();
-		WeaponId = SelectedEquip.EquippedId;
-		EquipWeapon();
+	void Start(){ // run at the start
+		WeaponId = SelectedEquip.EquippedId; // Determines the ID of the selected weapon
+		EquipWeapon(); // runs the EquipWeapon function
 	}
 	public void EquipWeapon(){
-		SqliteConnection WeaponDB = new SqliteConnection("Data Source=Assets\\Plugins\\WeaponsTable.db;Version=3;");
-		WeaponDB.Open();
-		string CMDString = "SELECT Category, Damage, Inaccuracy, Magazine, FireRate FROM tblWeapon WHERE id=@ID";
+		SqliteConnection WeaponDB = new SqliteConnection("Data Source=Assets\\Plugins\\WeaponsTable.db;Version=3;"); // Connects to DB
+		WeaponDB.Open(); // Opens the DB
+		string CMDString = "SELECT Category, Damage, Inaccuracy, Magazine, FireRate FROM tblWeapon WHERE id=@ID"; // Selects all the attributes of the weapons
 		SqliteCommand CMD = new SqliteCommand(CMDString,WeaponDB);
 		CMD.Parameters.AddWithValue("@id", SelectedEquip.EquippedId);
-		var reader = CMD.ExecuteReader();
-		Category = Convert.ToString(reader["Category"]);
+		var reader = CMD.ExecuteReader(); //executes the query 
+		Category = Convert.ToString(reader["Category"]); // Converts the arributes to their correct type
 		Damage = Convert.ToInt32(reader["Damage"]);
 		Inaccuracy = Convert.ToInt32(reader["Inaccuracy"]);
 		Magazine = Convert.ToInt32(reader["Magazine"]);
 		FireRate = Convert.ToInt32(reader["FireRate"]);
 		reader.Close();
 		WeaponDB.Close();
-		PlayerMovement.ChangeImageValue(Category);
+		PlayerMovement.ChangeImageValue(Category); // Changes the sprite value
 	}
 }
